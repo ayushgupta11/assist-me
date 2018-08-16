0743224280f4e98ffd2891790e2d8aa4a4e7348c
Ayush Gupta <ayushgupta@vijays-Mac-mini.local>
2018-08-16 19:04:21 +0530

    CSS addition update

--- main.js
+++ main.js
@@ -10,28 +10,28 @@
 		}
 		var buttonController = function(params){
 			var i = params.index
-			$(".skip").css("display","inline")
+			$(".assist-skip").css("display","inline")
 			if(i == elements.length){
-				$(".done").css("display", "inline")
-				$(".next").attr("disabled", "true")
-				$(".skip").css("display","none")
-				$(".back").removeAttr("disabled")
+				$(".assist-done").css("display", "inline")
+				$(".assist-next").css("display","none")
+				$(".assist-skip").css("display","none")
+				$(".assist-back").removeAttr("disabled")
 			}
 			else if(i==1){
-				$(".done").css("display", "none")
-				$(".back").attr("disabled", "true")
-				$(".next").removeAttr("disabled")
+				$(".assist-done").css("display", "none")
+				$(".assist-back").attr("disabled", "true")
+				$(".assist-next").removeAttr("disabled")
 			}
 			else{
-				$(".done").css("display", "none")
-				$(".next").removeAttr("disabled")
-				$(".back").removeAttr("disabled")
+				$(".assist-done").css("display", "none")
+				$(".assist-next").removeAttr("disabled")
+				$(".assist-back").removeAttr("disabled")
 			}
 			if(params.hasOwnProperty('hasEvent') && params.hasEvent){
-				$(".next").css("display", "none")
+				$(".assist-next").css("display", "none")
 			}
 			else{
-				$(".next").css("display", "inline")
+				$(".assist-next").css("display", "inline")
 			}
 		}
 		var showStep = function(i){
@@ -39,17 +39,24 @@
 			var params = {
 				index: i
 			}
+			var css = '';
 			$(element).append(step)
 			if(element.hasAttribute('data-has-event')){
 				params['hasEvent'] = $(element).attr('data-has-event')
 			}
+			if(element.hasAttribute('data-css')){
+				css = $(element).attr('data-css')
+			}
 			buttonController(params);
-			$(".show-step").removeAttr("style")
-			$(".show-step").removeClass("show-step")
-			$(".step_no").text($(element).attr('data-step'))
-			$(".overlay").css("display", "block")
-			$(element).addClass("show-step")
-			$("#step-content").html($(element).attr("data-content"))
+			$(".assist-show-step").removeAttr("style")
+			$(".assist-show-step").removeClass("assist-show-step")
+			$(".assist-step_no").text($(element).attr('data-step'))
+			$(".assist-overlay").css("display", "block")
+			$(element).addClass("assist-show-step")
+			if(css != ''){
+			$(".assist-show-step").attr('style', css)
+		}
+			$("#assist-step-content").html($(element).attr("data-content"))
 			$('html, body').animate({
 		        scrollTop: $(element).offset().top
 		    }, 500)
@@ -57,17 +64,17 @@
 		//Ends Here
 
 		//Plugin Controller
-		$(".overlay").remove()
-		var overlay = $("<div class='overlay'/>")
-		var step = $("<div class='step' />")
-		var content = $("<div id='step-content'/>")
-		var button_container = $("<div class='tour_content'/>")
-		var next = $("<button class='next'>Next</button>")
-		var back = $("<button class='back'>Back</button>")
-		var done = $("<button class='done'>Done</button>").css("display", "none")
-		var skip = $("<button class='skip'>Skip this Step</button>")
-		var skip_all = $("<button class='skip_all'>Skip Demo</button>")
-		var stepno = $("<span class='step_no' />")
+		$(".assist-overlay").remove()
+		var overlay = $("<div class='assist-overlay'/>")
+		var step = $("<div class='assist-step' />")
+		var content = $("<div id='assist-step-content'/>")
+		var button_container = $("<div class='assist-tour_content'/>")
+		var next = $("<button class='assist-next'>Next</button>")
+		var back = $("<button class='assist-back'>Back</button>")
+		var done = $("<button class='assist-done'>Done</button>").css("display", "none")
+		var skip = $("<button class='assist-skip'>Skip this Step</button>")
+		var skip_all = $("<button class='assist-skip_all'>Skip Demo</button>")
+		var stepno = $("<span class='assist-step_no' />")
 		$(step).append(content)
 		$(button_container).append(back)
 		$(button_container).append(next)
@@ -91,34 +98,35 @@
 		//Ends here
 
 		//Event Handlers
-		$(".next").on('click', function(){
+		$(".assist-next").on('click', function(){
 			if(index>0 && index<elements.length){
 			index++;
 		}
 			showStep(index)
 		})
-		$(".back").on('click', function(){
+		$(".assist-back").on('click', function(){
 			if(index>0 && index<=elements.length){
 			index--;
 			}
 			showStep(index)
 		})
-		$(".done").on("click", function(){
-			$(".overlay").css("display", "none")
-			$(".show-step").removeAttr("style")		
-			$(".show-step").removeClass("show-step")
-			$(".step").remove()
+		$(".assist-done").on("click", function(){
+			$(".assist-overlay").css("display", "none")
+			$(".assist-show-step").removeAttr("style")		
+			$(".assist-show-step").removeClass("assist-show-step")
+			$(".assist-step").remove()
 		})
-		$(".skip_all").on("click", function(){
-			$(".overlay").css("display", "none")
-			$(".show-step").removeAttr("style")		
-			$(".show-step").removeClass("show-step")
-			$(".step").remove()
+		$(".assist-skip_all").on("click", function(){
+			$(".assist-overlay").css("display", "none")
+			$(".assist-show-step").removeAttr("style")		
+			$(".assist-show-step").removeClass("assist-show-step")
+			$(".assist-step").remove()
 		})
-		$(".skip").on("click", function(){
-			if(elements[index-1].hasAttribute('data-branch')){
-				var countSteps = $("[data-branch=" + index + "]").length -1
-				if(index<elements.length){
+		$(".assist-skip").on("click", function(){
+			if(elements[index-1].hasAttribute('data-branch') && elements[index-1].hasAttribute('data-has-event')){
+				var branch_index = $(".assist-show-step").attr("data-branch")
+				var countSteps = $("[data-branch=" + branch_index + "]").length
+				if(index<elements.length && countSteps > 0){
 					index += countSteps
 				}
 			}
@@ -126,10 +134,10 @@
 			index++;
 		}
 		if(index > elements.length){
-			$(".overlay").css("display", "none")
-			$(".show-step").removeAttr("style")		
-			$(".show-step").removeClass("show-step")
-			$(".step").remove()
+			$(".assist-overlay").css("display", "none")
+			$(".assist-show-step").removeAttr("style")		
+			$(".assist-show-step").removeClass("assist-show-step")
+			$(".assist-step").remove()
 		}
 		else{
 			showStep(index)
